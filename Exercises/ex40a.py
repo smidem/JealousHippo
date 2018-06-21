673e3947b54d7a4e2d6592460d26d37ef8ee34c4
Shane McGuckian <shane.mcguckian@gmail.com>
2018-06-21 01:30:01 -0600

    added ex40a.py and mystuff.py as specified in Exercise 40

---
+++ Exercises/ex40a.py
@@ -0,0 +1,42 @@
+from mystuff import MyStuff
+# mystuff.apple()
+# print(mystuff.tangerine)
+
+# mystuff = {'apple': 'I AM APPLES!'}
+# print(mystuff['apple'])
+
+# this goes in mystuff.py
+# def apple():
+#     print('I AM APPLES!')
+
+# this is just a variable
+# tangerine = 'Living reflection of a dream'
+
+# mystuff['apple'] # get apple from dict
+# mystuff.apple() # get apple from the module
+# mystuff.tangerine # same thing, it's just a variable
+
+# class MyStuff(object):
+#
+#     def __init__(self):
+#         self.tangerine = 'And now a thousand years between'
+#
+#     def apple(self):
+#         print('I AM CLASSY APPLES!')
+
+# instantiating MyStuff class
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
+
+# dict style
+# mystuff['apple']
+
+# module style
+# mystuff.apple()
+# print(mystuff.tangerine)
+
+# class style
+# thing = MyStuff()
+# thing.apple()
+# print(thing.tangerine)
